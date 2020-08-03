819b038ee7640fa4d280c62b3b14940dfd32c69d
rseppi <rseppi@mpe.mpg.de>
2020-08-03 17:48:26 +0200

    output f already integrated

--- HMF_dyn_state_colossus/model_seppi20.py
+++ HMF_dyn_state_colossus/model_seppi20.py
@@ -1,5 +1,6 @@
 from colossus.lss import peaks
 import numpy as np
+from scipy import integrate
 
 
 def seppi20(sigma,xoff,spin,z):
@@ -7,24 +8,21 @@ def seppi20(sigma,xoff,spin,z):
 	The mass function model of Seppi et al 2020.
 	
 	The model is specified in Equation 23.
-    Needs a meshgrid of sigma,xoff,spin.
-    You can create it with numpy.meshgrid.
-    Make sure to use indexing 'ij'.
     Calibrated for M > 4x10^13 Msun at z=0.
 	
 	Parameters
 	-----------------------------------------------------------------------------------------------
 	sigma: array_like
-		Variance; can be a number or a meshgrid with xoff and spin.
+		Variance; can be a number or an array.
     xoff: array_like
-        Offset parameter; can be a number or a meshgrid with sigma and spin.
+        Offset parameter; can be a number or array.
     spin: array_like
-        Spin parameter; can be a number or a meshgrid with sigma and xoff.
+        Spin parameter; can be a number or an array.
 		
 	Returns
 	-----------------------------------------------------------------------------------------------
-	h: array_like
-		The halo mass-xoff-spin function :math:`h(\\sigma,xoff,spin)`, has the same dimensions as the input meshgrid.
+	f: array_like
+		The halo mass function :math:`f(\\sigma)`, integrated on the given values on xoff and spin. It has the same dimensions as the input meshgrid.
 	"""
     zp1 = 1+z
     dc = peaks.collapseOverdensity(z = 0)    
@@ -39,8 +37,26 @@ def seppi20(sigma,xoff,spin,z):
     delta = 1.209*(zp1)**-0.0768
     e = -1.105*(zp1)**0.6123
 
-    model_log = A+np.log10(np.sqrt(2/np.pi)) + q*np.log10(np.sqrt(a)*dc/sigma) - a/2/np.log(10)*dc**2/sigma**2 + (alpha)*np.log10(xoff/10**(1.83*mu)) - 1/np.log(10)*(xoff/10**(1.83*mu))**(0.05*alpha) + gamma*np.log10(spin/(10**(mu))) - 1/np.log(10)*(xoff/10**(1.83*mu)/sigma**e)**(beta)*(spin/(10**(mu)))**(delta)   
-    return 10**model_log
+    if xoff is None:
+        xoff = np.logspace(-3.5,-0.3,50)
+    if spin is None:
+        spin = np.logspace(-3.5,-0.3,50)
+
+    sigma_,xoff_,spin_ = np.meshgrid(sigma,xoff,spin,indexing='ij')
+
+    h_log = A+np.log10(np.sqrt(2/np.pi)) + q*np.log10(np.sqrt(a)*dc/sigma_) - a/2/np.log(10)*dc**2/sigma_**2 + (alpha)*np.log10(xoff_/10**(1.83*mu)) - 1/np.log(10)*(xoff_/10**(1.83*mu))**(0.05*alpha) + gamma*np.log10(spin_/(10**(mu))) - 1/np.log(10)*(xoff_/10**(1.83*mu)/sigma_**e)**(beta)*(spin_/(10**(mu)))**(delta)   
+    h = 10**h_log
+
+    g = np.zeros((len(sigma),len(xoff)))
+    for i in range(len(sigma)):
+        for j in range(len(xoff)):
+            g[i,j] = integrate.simps(h[i,j,:],np.log10(spin))
+
+    f = np.zeros(len(sigma))
+    for i in range(len(sigma)):
+        f[i] = integrate.simps(g[i,:],np.log10(xoff))
+    
+    return f
 
 
 
--- test.py
+++ test.py
@@ -4,7 +4,6 @@ Example script about seppi20 implementation in colossus
 
 #import model and integrals
 from HMF_dyn_state_colossus import model_seppi20
-from HMF_dyn_state_colossus import integrals
 
 #import colossus
 from colossus.cosmology import cosmology
@@ -22,24 +21,23 @@ cosmo = cosmology.setCosmology('multidark-planck')
 #define variance, xoff, spin arrays
 mass_ = np.logspace(13.5,16,60)
 R = peaks.lagrangianR(mass_)
-sigma_ = cosmo.sigma(R,z=0)
-#sigma_ = np.linspace(0.3,1.2,60)
-xoff_ = np.logspace(-3,-0.3,50)
-spin_ = np.logspace(-3.1,-0.4,40)
+sigma = cosmo.sigma(R,z=0)
 
-#create meshgrid
-sigma,xoff,spin = np.meshgrid(sigma_,xoff_,spin_,indexing='ij')
+xoff = np.logspace(-3.5,-0.3,50)
+spin = np.logspace(-3.5,-0.3,50)
+
+xoff2 = np.logspace(-3.5,-1.5,30)
+spin2 = np.logspace(-3.5,-1.5,30)
 
 #build model and integrate it
-h = model_seppi20.seppi20(sigma,xoff,spin,z=0)
-g = integrals.int_h_dspin(sigma_,xoff_,spin_,h)
-f_sigma = integrals.int_f_sigma(sigma_,xoff_,g)
+f_sigma = model_seppi20.seppi20(sigma,xoff,spin,z=0)
+f_sigma2 = model_seppi20.seppi20(sigma,xoff2,spin2,z=0)
 
 #use bhattacharya11, despali16 and comparat17 for comparison
-mf_tinker=mf.massFunction(sigma_,q_in='sigma', z=0, mdef = 'vir', model = 'tinker08', q_out = 'f') 
-mf_bhattacharya=mf.massFunction(sigma_,q_in='sigma', z=0, model = 'bhattacharya11', q_out = 'f') 
-mf_comparat=mf.massFunction(sigma_,q_in='sigma', z=0, mdef = 'vir', model = 'comparat17', q_out = 'f') 
-mf_despali=mf.massFunction(sigma_,q_in='sigma', z=0, mdef = 'vir', model = 'despali16', q_out = 'f') 
+mf_tinker=mf.massFunction(sigma,q_in='sigma', z=0, mdef = 'vir', model = 'tinker08', q_out = 'f') 
+mf_bhattacharya=mf.massFunction(sigma,q_in='sigma', z=0, model = 'bhattacharya11', q_out = 'f') 
+mf_comparat=mf.massFunction(sigma,q_in='sigma', z=0, mdef = 'vir', model = 'comparat17', q_out = 'f') 
+mf_despali=mf.massFunction(sigma,q_in='sigma', z=0, mdef = 'vir', model = 'despali16', q_out = 'f') 
 
 #function to convert log10(1/sigma) to Mass
 def Mass_sigma(x):
@@ -52,7 +50,7 @@ fig = plt.figure(figsize=(10,10))
 gs = fig.add_gridspec(3,1)
 ax1 = fig.add_subplot(gs[0:2, :])
 ax2 = fig.add_subplot(gs[2, :])
-x = np.log10(1/sigma_)
+x = np.log10(1/sigma)
 r1 = (f_sigma-mf_tinker)/mf_tinker
 r2 = (f_sigma-mf_bhattacharya)/mf_bhattacharya
 r3 = (f_sigma - mf_despali)/mf_despali
@@ -62,6 +60,7 @@ ax1.plot(x,mf_bhattacharya,label='bhattacharya11')
 ax1.plot(x,mf_despali,label='despali16')
 ax1.plot(x,mf_comparat,label='comparat17')
 ax1.plot(x,f_sigma,label='seppi20')
+ax1.plot(x,f_sigma2,label='seppi20 subsample')
 #ax1.plot(x,h[:,25,20],label='slice seppi20')
 ax2.plot(x,r1)
 ax2.plot(x,r2)
